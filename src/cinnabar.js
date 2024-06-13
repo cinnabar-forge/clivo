858ee85bc17f0ff5d8518b1bd66e5fad3bbe31e7
Timur Moziev <code@timurrin.ru>
2024-06-13 19:05:04 +0300

    release version 0.1.0

--- src/cinnabar.js
+++ src/cinnabar.js
@@ -15,10 +15,10 @@ export default {
   },
   "version": {
     "major": 0,
-    "minor": 0,
+    "minor": 1,
     "patch": 0,
-    "revision": null,
-    "text": "0.0.0",
-    "timestamp": 0
+    "revision": 1,
+    "text": "0.1.0",
+    "timestamp": 1718294648
   }
 };
